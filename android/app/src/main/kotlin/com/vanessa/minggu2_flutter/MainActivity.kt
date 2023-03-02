aa128e674b9b3df06dece4b547ea430261a47758
VFP <vanessa.florentina19@gmail.com>
2023-03-02 10:06:52 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/vanessa/minggu2_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vanessa.minggu2_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:minggu2_flutter/percobaan1.dart';
+import 'package:minggu2_flutter/percobaan2.dart';
+import 'package:minggu2_flutter/percobaan3.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Praktikum 2'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Percobaan 1'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PercobaanSatu()),
+                );
+              },
+            ),
+            // ElevatedButton(
+            //   child: const Text('Percobaan 2'),
+            //   onPressed: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(builder: (context) => const PercobaanDua()),
+            //     );
+            //   },
+            // ),
+            // ElevatedButton(
+            //   child: const Text('Percobaan 3'),
+            //   onPressed: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(builder: (context) => const PercobaanTiga()),
+            //     );
+            //   },
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
